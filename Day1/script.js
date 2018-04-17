49ab70ce6b4cfbc791cf1067c886e0902c2e68bc
Opeezy <copenshaw17@gmail.com>
2018-04-17 17:01:03 -0500

    first day submit

---
+++ Day1/script.js
@@ -0,0 +1,28 @@
+console.time('Day1 Challenge');
+
+const fs = require('fs');
+
+const startFloor = 0;
+let floorUp = 0;
+let floorDown = 0;
+
+
+fs.readFile('./input.txt', 'utf-8', (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		const directions = data.split("");
+		directions.forEach(direction => {
+			if (direction === '(') {
+				floorUp++;
+			} else if (direction === ')') {
+				floorDown++;
+			} else {
+				return
+			}			
+		})
+		console.log(startFloor + floorUp - floorDown);
+	}
+})
+	
+console.timeEnd('Day1 Challenge');
